b0e4c054446b637ca5a9c3a68b22bef4197095d9
aram özer <aramozer@gmail.com>
2022-06-09 21:43:48 +0200

    2.8.4 - adding comments above each function

--- dict.py
+++ dict.py
@@ -6,24 +6,32 @@ conn = psycopg2.connect(
    password="password"
 )
 
+# read_dict: returns the list of all dictionary entries:
+# argument: C - the database connection.
 def read_dict(C):
     cur = C.cursor()
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
     return rows
+# add_word: INSERTS new words in the dictionary:
+# arguments: word and translation are each column in the database
 def add_word(C, word, translation):
     cur = C.cursor()
     cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
     cur.close()
+# delete_word: DELETS a row in the dictionary:
+# argumant: ID is the value of id.
 def delete_word(C, ID):
     cur = C.cursor()
     cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
     cur.close()
+# save_dict: makes a commit to database.
 def save_dict(C):
     cur = C.cursor()
     cur.execute("COMMIT;")
     cur.close()
+# help_list
 def help_list():
     print(list_of_inst)
 
